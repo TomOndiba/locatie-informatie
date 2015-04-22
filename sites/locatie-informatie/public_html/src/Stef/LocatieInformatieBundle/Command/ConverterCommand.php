bddc9e55aed589a6e96e5f25d194e3a15c2118a6
stefanius <KIT_kat_16>
2015-04-23 00:27:58 +0200

    Updated bundle and defautController, added a convert Command. Updated fixtures.

---
+++ sites/locatie-informatie/public_html/src/Stef/LocatieInformatieBundle/Command/ConverterCommand.php
@@ -0,0 +1,39 @@
+<?php
+namespace Stef\LocatieInformatieBundle\Command;
+
+use Stef\LocatieInformatieBundle\Conversion\CityConverter;
+use Stef\LocatieInformatieBundle\Conversion\MunicipalityConverter;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ConverterCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('location:convert')
+            ->setDescription('Convert Locationdata in our own format.')
+        ;
+    }
+
+    protected function get($service)
+    {
+        return $this->getApplication()->getKernel()->getContainer()->get($service);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $municipalityConverter = new MunicipalityConverter($this->get('stef_simple_cms.postcode_manager'), $this->get('stef_simple_cms.municipality_manager'), $this->get('stef.slugifier'));
+        $municipalityConverter->convert();
+
+        $cityConverter = new CityConverter($this->get('stef_simple_cms.postcode_manager'), $this->get('stef_simple_cms.municipality_manager'), $this->get('stef_simple_cms.city_manager'), $this->get('stef.slugifier'));
+        $cityConverter->convert();
+        var_dump('erte6y4y');
+
+
+
+    }
+}
