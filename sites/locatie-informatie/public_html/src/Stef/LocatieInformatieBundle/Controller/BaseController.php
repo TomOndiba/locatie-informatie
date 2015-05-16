aec227e8e09e6da082623beeb15cdc80fdfe7085
stefanius <KIT_kat_16>
2015-05-16 15:07:06 +0200

    Updated routing, added homepage, added and updated twig, added basic indexpaged for three types of locations.

---
+++ sites/locatie-informatie/public_html/src/Stef/LocatieInformatieBundle/Controller/BaseController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Stef\LocatieInformatieBundle\Controller;
+
+use Stef\LocatieInformatieBundle\Manager\CityManager;
+use Stef\LocatieInformatieBundle\Manager\MunicipalityManager;
+use Stef\LocatieInformatieBundle\Manager\ProvinceManager;
+use Stef\SimpleCmsBundle\Manager\PageManager;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BaseController extends Controller
+{
+    /**
+     * @return CityManager
+     */
+    public function getCityManager()
+    {
+        return $this->get('stef_simple_cms.city_manager');
+    }
+
+    /**
+     * @return MunicipalityManager
+     */
+    public function getMunicipalityManager()
+    {
+        return $this->get('stef_simple_cms.municipality_manager');
+    }
+
+    /**
+     * @return ProvinceManager
+     */
+    public function getProvinceManager()
+    {
+        return $this->get('stef_simple_cms.province_manager');
+    }
+
+    /**
+     * @return PageManager
+     */
+    public function getPageManager()
+    {
+        return $this->get('stef_simple_cms.page_manager');
+    }
+}
