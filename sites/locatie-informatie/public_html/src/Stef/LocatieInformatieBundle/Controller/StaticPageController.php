39c73d3d01ea3ffb2de6818978d0517fbde6bbeb
stefanius <KIT_kat_16>
2015-05-25 16:22:48 +0200

    Open Friesland

--- sites/locatie-informatie/public_html/src/Stef/LocatieInformatieBundle/Controller/StaticPageController.php
+++ sites/locatie-informatie/public_html/src/Stef/LocatieInformatieBundle/Controller/StaticPageController.php
@@ -58,6 +58,10 @@ class StaticPageController extends BaseController
             $page->setRobotsIndex(true);
         }
 
+        if ($province != null && $province->getProvinceCode() == 'FR') {
+            $page->setRobotsIndex(true);
+        }
+
         if ($provinceSlug == null && $province == null) {
             $page->setRobotsIndex(true);
         }
