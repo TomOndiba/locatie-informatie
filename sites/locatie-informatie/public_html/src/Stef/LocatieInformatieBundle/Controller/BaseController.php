3ba583491abdef8497d52b1f912f7fae6ceeb96c
stefanius <KIT_kat_16>
2015-06-22 00:18:59 +0200

    Adding Zipcode page, routing, controller and view.

--- sites/locatie-informatie/public_html/src/Stef/LocatieInformatieBundle/Controller/BaseController.php
+++ sites/locatie-informatie/public_html/src/Stef/LocatieInformatieBundle/Controller/BaseController.php
@@ -5,6 +5,7 @@ namespace Stef\LocatieInformatieBundle\Controller;
 use Stef\LocatieInformatieBundle\Manager\CityManager;
 use Stef\LocatieInformatieBundle\Manager\MunicipalityManager;
 use Stef\LocatieInformatieBundle\Manager\ProvinceManager;
+use Stef\LocatieInformatieBundle\Manager\ZipcodeManager;
 use Stef\SimpleCmsBundle\Manager\PageManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -32,6 +33,14 @@ class BaseController extends Controller
     public function getProvinceManager()
     {
         return $this->get('stef_simple_cms.province_manager');
+    }
+
+    /**
+     * @return ZipcodeManager
+     */
+    public function getZipcodeManager()
+    {
+        return $this->get('stef_simple_cms.zipcode_manager');
     }
 
     /**
